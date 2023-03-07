cd1da12cb5266a56831c2e7c99fe6d7374965c46
abbywonka <71005559+abbywonka@users.noreply.github.com>
2023-03-07 20:18:51 +0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,34 @@
+const parser = new DOMParser();
+
+const xmlString = `
+<list>
+<student>
+  <name lang="en">
+    <first>Ivan</first>
+    <second>Ivanov</second>
+  </name>
+  <age>35</age>
+  <prof>teacher</prof>
+</student>
+<student>
+  <name lang="ru">
+  const jsonString = `
+  {
+  "list": [
+   {
+    "name": "Petr",
+    "age": "20",
+    "prof": "mechanic"
+   },
+   {
+    "name": "Vova",
+    "age": "60",
+    "prof": "pilot"
+   }
+  ]
+ }
+`;
+
+let data = JSON.parse(jsonString);
+
+console.log(data)
